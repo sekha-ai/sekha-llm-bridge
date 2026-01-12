fc3d626cba09d0e405bfc855a99d719c51f77831
jefftraylor <jeff.traylor@c9operations.com>
2026-01-12 11:58:53 -0500

    Update models __init__ to export chat completion models

--- src/sekha_llm_bridge/models/__init__.py
+++ src/sekha_llm_bridge/models/__init__.py
@@ -1,14 +1,18 @@
 from sekha_llm_bridge.models.requests import (
-    EmbedRequest, SummarizeRequest, ExtractRequest, ScoreRequest
+    EmbedRequest, SummarizeRequest, ExtractRequest, ScoreRequest,
+    ChatCompletionRequest, ChatMessage
 )
 
 from sekha_llm_bridge.models.responses import (
     EmbedResponse, SummarizeResponse, ExtractResponse,
-    ScoreResponse, HealthResponse
+    ScoreResponse, HealthResponse,
+    ChatCompletionResponse, ChatCompletionChoice, ChatCompletionUsage
 )
 
 __all__ = [
     'EmbedRequest', 'SummarizeRequest', 'ExtractRequest', 'ScoreRequest',
+    'ChatCompletionRequest', 'ChatMessage',
     'EmbedResponse', 'SummarizeResponse', 'ExtractResponse',
-    'ScoreResponse', 'HealthResponse'
+    'ScoreResponse', 'HealthResponse',
+    'ChatCompletionResponse', 'ChatCompletionChoice', 'ChatCompletionUsage'
 ]
